44594a14073a130e0c6fefb991a3351a9d33d464
rahma-abdelwahab <rahmaabdelwahab27@gmail.com>
2026-03-27 21:40:34 +0200

    test: try to get the accuracy < 80

--- train.py
+++ train.py
@@ -24,9 +24,8 @@ def main():
     X = df.drop(columns=["target"])
     y = df["target"]
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42
-    )
-
+    X, y, test_size=0.99, random_state=42
+)
     # 2. Train
     model = SVC(kernel="linear", C=0.001, random_state=42)
     model.fit(X_train, y_train)
